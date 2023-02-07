5c8e806f61adc502e791b49326d90b89f5bf9800
Pierre <Pierre_gourgouillon@hotmail.com>
2023-02-07 16:32:15 +0100

    Initial Commit

---
+++ spotify-portfolio/spotify_portfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotify_portfolioApp.swift
+//  spotify-portfolio
+//
+//  Created by Pierre Gourgouillon on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct spotify_portfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
